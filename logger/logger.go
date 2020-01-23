f442912295ac09a42b549d9e1df0be3a8376daf2
akakou <akakou571@gmail.com>
2020-01-23 23:22:26 +0900

    [add] Distribute with shamir secret sharing

--- logger/logger.go
+++ logger/logger.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/itslab-kyushu/sss/sss"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	chunksize := 128
+	totalShares := 10
+	threshold := 5
+
 	var buffer bytes.Buffer
 
 	r.Write(&buffer)
-	fmt.Print(buffer.String())
+	shares, _ := sss.Distribute(buffer.Bytes(), chunksize, totalShares, threshold)
+
+	for _, s := range shares {
+		data, err := json.Marshal(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Print(string(data))
+	}
 
 	fmt.Fprintf(w, "ok")
 }
