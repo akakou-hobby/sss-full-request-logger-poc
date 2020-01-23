b0b9447b11088088cc88637ea26d2df51654f1aa
akakou <akakou571@gmail.com>
2020-01-24 01:10:55 +0900

    [add] Read yaml config

--- logger/logger.go
+++ logger/logger.go
@@ -5,14 +5,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/itslab-kyushu/sss/sss"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type Config struct {
+	Stores []string `stores`
+	Threshold int `threshold`
+}
+
+var config Config
+var stores []string
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	chunksize := 128
 	totalShares := 10
-	threshold := 5
+    threshold := config.Threshold
 
 	var buffer bytes.Buffer
 
@@ -32,6 +42,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	buf, _ := ioutil.ReadFile("config.yaml")
+	yaml.Unmarshal(buf, &config)
+
+	fmt.Printf("--- config:\n%v\n\n", config.Stores)
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
