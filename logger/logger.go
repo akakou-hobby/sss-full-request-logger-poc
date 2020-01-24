b0b4bcd0c4755b0bfac9d1a0362d1f8301799083
akakou <akakou571@gmail.com>
2020-01-24 14:39:30 +0900

    [add] Enctypt and dectypto with RSA

--- logger/logger.go
+++ logger/logger.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"github.com/itslab-kyushu/sss/sss"
 	"gopkg.in/yaml.v2"
@@ -17,6 +22,12 @@ type Config struct {
 
 var config Config
 var stores []string
+
+var prikeyFile = "keys/prikey.pem"
+var pubkeyFile = "keys/pubkey.pem"
+
+var prikey *rsa.PrivateKey
+var pubkey *rsa.PublicKey
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	stores = config.Stores
@@ -46,7 +57,26 @@ func main() {
 	buf, _ := ioutil.ReadFile("config.yaml")
 	yaml.Unmarshal(buf, &config)
 
-	fmt.Printf("--- config:\n%v\n\n", config.Stores)
+	// fmt.Printf("--- config:\n%v\n\n", config.Stores)
+
+	bytes, _ := ioutil.ReadFile(pubkeyFile)
+	block, _ := pem.Decode(bytes)
+	_pubkey, _ := x509.ParsePKIXPublicKey(block.Bytes)
+	pubkey = _pubkey.(*rsa.PublicKey)
+
+	label := []byte("label")
+	message := []byte("message")
+
+	ciphertext, _ := rsa.EncryptOAEP(sha256.New(), rand.Reader, pubkey, message, label)
+
+	{
+		bytes, _ := ioutil.ReadFile(prikeyFile)
+		block, _ := pem.Decode(bytes)
+		prikey, _ = x509.ParsePKCS1PrivateKey(block.Bytes)
+		plaintext, _ := rsa.DecryptOAEP(sha256.New(), rand.Reader, prikey, ciphertext, label)
+
+		fmt.Print(string(plaintext))
+	}
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
